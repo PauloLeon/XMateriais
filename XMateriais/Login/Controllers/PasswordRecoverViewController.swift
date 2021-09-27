58a29ee016c5ad0bfdcaa53db789d3d8c458697e
Paulo Rosa <pleonrosa@gmail.com>
2021-09-27 16:27:47 -0300

    better code layout

--- XMateriais/Login/Controllers/PasswordRecoverViewController.swift
+++ XMateriais/Login/Controllers/PasswordRecoverViewController.swift
@@ -65,7 +65,6 @@ extension PasswordRecoverViewController: UITextFieldDelegate {
     
     func isValidEmail(testStr:String) -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        
         let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
         return emailTest.evaluate(with: testStr)
     }
--- XMateriais/Login/Controllers/RegistrationResearcherViewController.swift
+++ XMateriais/Login/Controllers/RegistrationResearcherViewController.swift
@@ -143,7 +143,6 @@ extension RegistrationResearcherViewController: UITextFieldDelegate {
     
     func isValidEmail(testStr:String) -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        
         let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
         return emailTest.evaluate(with: testStr)
     }
